40a4e27c61f32db7e8a4ace76abad0f8dbac27cd
Kunal <rails@kunalinfotech.net>
2011-11-30 16:41:07 +0530

    user model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,40 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :first_name, :last_name, :username
+  
+  def apply_omniauth(omniauth)
+    case omniauth['provider']
+    when 'facebook'
+      self.apply_facebook(omniauth)
+    end
+    authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
+  end 
+  
+  
+  def facebook
+    @fb_user ||= FbGraph::User.me(self.authentications.find_by_provider('facebook').token)
+  end
+
+  def password_required?
+    (authentications.empty? || !password.blank?) && super  
+    end                                                
+         
+      def update_with_password(params={}) 
+      if params[:password].blank? 
+        params.delete(:password) 
+        params.delete(:password_confirmation) if 
+    params[:password_confirmation].blank? 
+      end 
+
+      update_attributes(params) 
+    end
+     
+         protected
+
+    def apply_facebook(omniauth)
+      if (extra = omniauth['extra']['user_hash'] rescue false)
+        self.email = (extra['email'] rescue '')
+      end
+    end
+    
 end
