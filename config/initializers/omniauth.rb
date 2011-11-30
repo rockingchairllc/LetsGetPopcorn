f169d71b821ffd04594928e091a21747292b9eff
Kunal <rails@kunalinfotech.net>
2011-11-30 16:35:07 +0530

    facebook key updated

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   
-  provider :facebook,'146261058813526', '59252c9bf43e22a8bdff70f5e030cd84',{:scope => 'publish_stream,offline_access,email'}
+  provider :facebook,'191637837589329', '3f7d39f10457e62a53d2bf2825002010',{:scope => 'publish_stream,offline_access,email'}
     
 end
