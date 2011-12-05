455d59fb5ea43ecf41b7a74dd00766bf92bb9205
Kunal <rails@kunalinfotech.net>
2011-12-05 12:51:03 +0530

    update enviorment file

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Moviewithme::Application.initialize!
+
+OmniAuth.config.on_failure = Proc.new do |env|
+
+end
