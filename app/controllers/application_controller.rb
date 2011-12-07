68cd37cfdba77cbf467ec0ce51bce8aa92d26350
Kunal <rails@kunalinfotech.net>
2011-12-07 11:01:29 +0530

    removed http request to redirect on www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  before_filter :strip_www
-
-  def strip_www
-    if request.env["HTTP_HOST"] == "www.letsgetpopcorn.com"
-      redirect_to "http://letsgetpopcorn.com/"
-    end
-  end
-    
 end
