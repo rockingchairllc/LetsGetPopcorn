a69c1482efbb626e5cc4c7e4ec17e7a01d5542de
Kunal <rails@kunalinfotech.net>
2011-12-07 10:47:20 +0530

    http request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
   
   before_filter :ensure_domain
 
-    APP_DOMAIN = 'www.letsgetpopcorn.com'
+    APP_DOMAIN = 'letsgetpopcorn.com'
 
     def ensure_domain
       if request.env['HTTP_HOST'] != APP_DOMAIN
         # HTTP 301 is a "permanent" redirect
-        redirect_to "http://#{APP_DOMAIN}", :status => 301
+        redirect_to "http://www.#{APP_DOMAIN}", :status => 301
       end
     end
     
