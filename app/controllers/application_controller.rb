f3caeb344ca4b41faa52a799f34eebb0e10f6124
Kunal <rails@kunalinfotech.net>
2011-12-07 10:54:26 +0530

    http request updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  redirect_to "www.letsgetpopcorn.com"
+  before_filter :strip_www
+
+  def strip_www
+    if request.env["HTTP_HOST"] == "letsgetpopcorn.com"
+      redirect_to "http://www.letsgetpopcorn.com/"
+    end
+  end
     
 end
