c66b2e6aef1926b6d186f23e1367b5eb49bee785
Kunal <rails@kunalinfotech.net>
2011-12-19 16:48:37 +0530

    user model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,8 @@ class User < ActiveRecord::Base
   has_attached_file :profile_photo, 
         :styles => { :thumb => "80x75" },
         
-        
+        :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+        :path => "user_photo/:id/:style/:basename.:extension",
         :convert_options => {
               :thumb => "-background '#F7F4F4' -compose Copy -gravity center -extent 80x75"
           }
