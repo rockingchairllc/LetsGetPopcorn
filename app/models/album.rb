e5b2e34f0b755bf644f55c046c5e776c5e161e9e
Amol <rails@kunalinfotech.net>
2012-01-27 10:04:54 +0530

    s3 path

--- app/models/album.rb
+++ app/models/album.rb
@@ -4,7 +4,8 @@ class Album < ActiveRecord::Base
   
   has_attached_file :photo, 
         :styles => { :thumb => "80x75" },
-        
+        :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+        :path => "reel/:id/:style/:basename.:extension",
         :convert_options => {
               :thumb => "-background '#444444' -compose Copy -gravity center -extent 80x75"
           }
